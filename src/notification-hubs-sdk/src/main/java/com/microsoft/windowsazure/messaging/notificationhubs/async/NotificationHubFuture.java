93b1d698f7b227b16551756cdb9235d2747798e7
Matthew Podwysocki <matthewp@microsoft.com>
2020-03-26 14:20:59 -0400

    Add basic futures

---
+++ src/notification-hubs-sdk/src/main/java/com/microsoft/windowsazure/messaging/notificationhubs/async/NotificationHubFuture.java
@@ -0,0 +1,21 @@
+package com.microsoft.windowsazure.messaging.notificationhubs.async;
+
+public interface NotificationHubFuture<T> {
+
+    /**
+     * Waits if necessary for the computation to complete, and then
+     * retrieves its result.
+     *
+     * @return the computed result.
+     */
+    T get();
+
+    /**
+     * Execute the consumer once the computation is completed with the result.
+     * The consumer function is called in the U.I. thread.
+     *
+     * @param function the action to perform upon completion.
+     */
+    void thenAccept(NotificationHubConsumer<T> function);
+
+}
